82687f77e3662b30d9856e044da4735cb73d9c33
rodzarayai <rodz.arayai@gmail.com>
2024-03-10 20:39:07 +1100

    init

---
+++ app/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+from joblib import load
+
+
+
+st.header('36105 iLab: Capstone Project - Autumn 2024 - UTS)
+# Title
+st.title('How lifestyle/habits can lead to obesity and diabetes type 2')
+          
+          
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='iLab Capstone Project: How lifestyle/habit can lead to obesity and diabetes type 2',
+    author='Group 6',
+    license='MIT',
+)
