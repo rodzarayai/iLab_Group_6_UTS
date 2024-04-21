75796310b7d81b57dfb87f6f70844314c574edf2
amansinghdalal <128581804+amansinghdalal@users.noreply.github.com>
2024-04-21 20:51:36 +1000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -156,10 +156,10 @@ def page_home():
     # Centered title using markdown and HTML
     # Centered titles
     st.markdown("<h1 style='text-align: center; font-family: Segoe UI, sans-serif'>LiveWell 🌱</h1>", unsafe_allow_html=True)
-    st.markdown("<h2 style='text-align: center; font-family: Segoe UI, sans-serif'>Obesity Prevention & Diabetes Learning Platform 📚</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; font-family: Segoe UI, sans-serif; font-size: 20px'>Obesity Prevention & Diabetes Learning Platform 📚</h2>", unsafe_allow_html=True)
 
     # Aligned headers
-    st.markdown("<h3 style='text-align: justify; font-family: Segoe UI, sans-serif'>Obesity is a major risk factor for a range of diseases, including heart disease, stroke, diabetes, and various types of cancer.</h3>", unsafe_allow_html=True)
+    st.markdown("'''<h3 style='text-align: justify; font-family: Segoe UI, sans-serif'>'''Obesity is a major risk factor for a range of diseases, including heart disease, stroke, diabetes, and various types of cancer.</h3>", unsafe_allow_html=True)
     st.markdown("""
             <h3 style='text-align: justify; font-family: Segoe UI, sans-serif; font-size: 20px'>
             The diabetes epidemic is one of the largest and most complex health challenges Australia has faced. 
