4155ff27b869aad5542e36d35520f44f2b3d926a
rodzarayai <rodz.arayai@gmail.com>
2024-04-28 19:29:04 +1000

    app  logo

--- app/app.py
+++ app/app.py
@@ -178,7 +178,7 @@ def page_home():
     # Title
     left_co, cent_co, last_co = st.columns(3)
     with cent_co:
-        st.image('healthscope_transparent.png', width=400)
+        st.image('app_logo.png', width=400)
 
 
     # Aligned headers
